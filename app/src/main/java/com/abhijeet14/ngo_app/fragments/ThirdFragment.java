ee61742f08321ea5c2c8bfd129289900dfc18484
abhijeet1410 <42213151+abhijeet1410@users.noreply.github.com>
2018-12-09 10:27:16 +0530

    Form UI done

--- app/src/main/java/com/abhijeet14/ngo_app/fragments/ThirdFragment.java
+++ app/src/main/java/com/abhijeet14/ngo_app/fragments/ThirdFragment.java
@@ -3,6 +3,9 @@ package com.abhijeet14.ngo_app.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +16,9 @@ import com.abhijeet14.ngo_app.R;
  * A simple {@link Fragment} subclass.
  */
 public class ThirdFragment extends Fragment {
-
-
+    private View v;
+    private Fragment f = null;
+    private AppCompatButton nextBtn;
     public ThirdFragment() {
         // Required empty public constructor
     }
@@ -24,7 +28,19 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+       v = inflater.inflate(R.layout.fragment_third, container, false);
+        nextBtn = v.findViewById(R.id.third_fragment_next_btn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                f = new FourthFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.fragment_container,f);
+                ft.commit();
+            }
+        });
+       return v;
     }
 
 }
